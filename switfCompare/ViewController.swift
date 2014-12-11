5993674560217cea22b95ab19047bb7b8435d194
yuhailong <yuhailong@yuhailongdeMacBook-Pro.local>
2014-12-11 08:53:14 +0800

    Initial Commit

---
+++ switfCompare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  switfCompare
+//
+//  Created by yuhailong on 14-12-11.
+//  Copyright (c) 2014年 polystor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
